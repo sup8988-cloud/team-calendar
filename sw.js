2dda77b5e2efd790dc6a1025e3622a568278659f
sup8988-cloud <sup8988@gmail.com>
2026-08-08 16:03:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="team-calendar-v2.4.0";
+const CACHE_NAME="team-calendar-v2.5.0";
 const APP_SHELL=["./","./index.html","./style.css","./app.js","./password-change.js","./admin-tools.js","./manifest.json","./og-image.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
